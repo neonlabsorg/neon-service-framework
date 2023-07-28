6763cff39788fdc595ee04a61777e237f05903dd
Vladimir Bugaev <boogvar@gmail.com>
2023-07-28 14:38:48 +0700

    api servers processing

---
+++ pkg/service/configuration/errors.go
@@ -0,0 +1,8 @@
+package configuration
+
+import "github.com/neonlabsorg/neon-service-framework/pkg/errors"
+
+var (
+	ErrTryingToUseUnitedApiServerName   = errors.Critical.NewWithCode(100102001, "trying to use united API server name")
+	ErrListenAddressForAPIServerisEmpty = errors.Critical.NewWithCode(100102002, "listen address for API server is empty")
+)
